0dd2c217df8bb13e946b75dea4955f5761f8f0ec
avikam03 <avikam03@gmail.com>
2023-05-13 21:40:25 -0400

    after applying fix that didn't work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,82 +1,63 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 import { useState, useEffect } from 'react';
-import storage from "../../public/storage"
 
-// import { WeekGraph } from '../components/weekgraph'
-// import WeekGraph from '../components/weekgraph'
-// import DayGraph from '../components/daygraph';
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 type ScreenTime = {
-	id: string;
-	url: string;
-	title: string;
-	startTime: Date;
-	endTime: Date;
+  id: string;
+  url: string;
+  title: string;
+  startTime: Date;
+  endTime: Date;
 };
 
 export default function Home() {
-  // var loading: boolean = false;
   const [loading, setLoading] = useState(false);
-  // var processedData: {[key: string]: number} = {};
-  const [processedData, setProcessedData] = useState({} as {[key: string]: number}); // [hostname: string]: number
-  
-  // write a function to get the data from chrome.storage then process it into a graph
+  const [processedData, setProcessedData] = useState({} as { [key: string]: number });
+
   useEffect(() => {
-    console.log("useEffect() in index.tsx")
-    // loading = true;
     setLoading(true);
-    
-    var rawData: ScreenTime[] = [];
 
-    storage.get("limitify_raw").then((result: ScreenTime[]) => {
-      console.log("limitify_raw just got getted")
-      rawData = result;
-      console.log("rawData:" + JSON.stringify(rawData))
-    })
-    storage.set("limitify_raw", [])
+    // Run the code only on the client-side
+    if (typeof window !== 'undefined') {
+      import('../../public/storage.js').then((storage) => {
+        var rawData: ScreenTime[] = [];
 
-    storage.get("limitify_processed").then((result: {[key: string]: number}) => {
-      console.log("limitify_processed just got getted")
-      console.log("processed:" + JSON.stringify(result))
+        storage.default.get('limitify_raw').then((result: ScreenTime[]) => {
+          rawData = result;
+          console.log('rawData:', JSON.stringify(rawData));
+        });
 
-      rawData.forEach(element => {
-        console.log("element:" + JSON.stringify(element))
-        console.log("element.url is: " + JSON.stringify(element.url))
-        var date1 = new Date(element.endTime)
-        var date2 = new Date(element.startTime)
-        // result[element.url.hostname] += Math.abs(date1.getTime() - date2.getTime()) / 1000;
-        // console.log("Just added " + Math.abs(date1.getTime() - date2.getTime()) / 1000 + " seconds to " + element.url.hostname);
-        if (result[element.url] == undefined) {
-          result[element.url] = 0;
-        }
-        result[element.url] += Math.abs(date1.getTime() - date2.getTime()) / 1000;
-        console.log("Just added " + Math.abs(date1.getTime() - date2.getTime()) / 1000 + " seconds to " + element.url);
-      })
+        storage.default.set('limitify_raw', []);
 
-      var sortedData = Object.entries(result).sort((a, b) => b[1] - a[1]);
-      result = Object.fromEntries(sortedData);
+        storage.default.get('limitify_processed').then((result: { [key: string]: number }) => {
+          console.log('processed:', JSON.stringify(result));
 
-      storage.set("limitify_processed", result);
-      // processedData = result;
-      setProcessedData(result)
-      // loading = false;
-      setLoading(false);
-    })
+          rawData.forEach((element) => {
+            var date1 = new Date(element.endTime);
+            var date2 = new Date(element.startTime);
+            if (result[element.url] === undefined) {
+              result[element.url] = 0;
+            }
+            result[element.url] += Math.abs(date1.getTime() - date2.getTime()) / 1000;
+            console.log(`Just added ${Math.abs(date1.getTime() - date2.getTime()) / 1000} seconds to ${element.url}`);
+          });
 
+          var sortedData = Object.entries(result).sort((a, b) => b[1] - a[1]);
+          result = Object.fromEntries(sortedData);
+
+          storage.default.set('limitify_processed', result);
+          setProcessedData(result);
+          setLoading(false);
+        });
+      });
+    }
   }, []);
 
-
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
-    >
-      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]"> */}
-      {/* <div className="place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]"> */}
+    <main className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}>
       <body className="px-4 bg-gradient-to-br from-stone-600 via-black to-slate-700">
-      {/* h-max w-full */}
 
         {loading ? <h2 className="mt-4 text-4xl font-extrabold dark:text-white">Loading...</h2>
                  : <h2 className="mt-4 text-xl font-extrabold dark:text-white">Screen time for today</h2>
@@ -122,8 +103,6 @@ export default function Home() {
                               ? Math.round(processedData[key] / 60) + "min" // If time is more than 1 minute
                               : Math.round(processedData[key]) + "s" // If time is less than one minute
                           }
-
-                          {/* {Math.round(processedData[key])} */}
                         </td>
                         <td className="whitespace-nowrap px-6 py-4"></td>
                       </tr>
