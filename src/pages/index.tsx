6c63fd280d4ddb293df5dd828374e3b5a87db31b
avikam03 <avikam03@gmail.com>
2023-07-02 15:42:23 -0400

    fixed toggle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -232,7 +232,7 @@ export default function Home() {
                                     checked={blockedData[key]}
                                     onChange={handleToggle(key)}
                                   />
-                                  <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                  <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                                 </label>
                               </td>
                             ) : null}
